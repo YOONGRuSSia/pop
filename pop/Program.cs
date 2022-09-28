d72dc785d8d648a731a8ee7cefa764277ea4c5b2
YOONGRuSSia <romanov.k.2019@mail.ru>
2022-09-28 08:58:32 +0300

    1

--- pop/Program.cs
+++ pop/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("HEllo, brazil");
+Console.WriteLine("HEllo, biba");
+Console.WriteLine("HEllo, boba");
